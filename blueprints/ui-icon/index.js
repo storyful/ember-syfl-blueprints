3553bfdcca03dc99c2f1ec57e076f0cdf52d0eb6
Aoife Hannigan <aoife.hannigan@storyful.com>
2018-11-08 14:35:01 +0000

    Resort icons alphabetically after new icon added

--- blueprints/ui-icon/index.js
+++ blueprints/ui-icon/index.js
@@ -1,5 +1,7 @@
 /* eslint-env node */
-const fs = require('fs');
+const fs = require('fs'),
+    yaml = require('js-yaml');
+
 const ICON_FILE = './addon/config/icons.yaml';
 
 module.exports = {
@@ -37,7 +39,7 @@ module.exports = {
     
   },
 
-  beforeInstall(options) {
+  createIconString(options){
     const newIconName = options.entity.name;
     const iconFontName = options.fontName;
     const modifier = options.modifier;
@@ -53,19 +55,43 @@ module.exports = {
       return;
     }
 
-    let text = `\n${newIconName}:`;
+    let iconString = `\n${newIconName}:`;
     if(isSyflIcon){
-      text = `${text}\n  name: ${newIconName}\n  font-prefix: syfl-icon\n  font: syfl-icon-font`;
+      iconString = `${iconString}\n  name: ${newIconName}\n  font-prefix: syfl-icon\n  font: syfl-icon-font`;
     }
     else{
-      text = `${text}\n  name: ${iconFontName}`;
+      iconString = `${iconString}\n  name: ${iconFontName}`;
       if(modifier){
-        text = `${text}\n  modifier: ${modifier}`;
+        iconString = `${iconString}\n  modifier: ${modifier}`;
       }
     }
 
-    fs.appendFileSync(ICON_FILE, text);
+    return iconString;
+  },
+
+  sortIcons(){
+    const jsonContent = yaml.load(fs.readFileSync(ICON_FILE, {encoding: 'utf-8'}));
+    let sortedJsonContent = {};
+
+    // sort icons alphabetically
+    Object.keys(jsonContent).sort().forEach(function(key) {
+      sortedJsonContent[key] = jsonContent[key];
+    });
+
+    return sortedJsonContent;
+  },
+
+  beforeInstall(options) {
+    const newIconName = options.entity.name;
+    const iconString = this.createIconString(options);
+    // add new icon to yaml file
+    fs.appendFileSync(ICON_FILE, iconString);
+    
+    const sortedIcons = this.sortIcons();
+    // overwrite file by adding alphabetically sorted icons 
+    fs.writeFileSync(ICON_FILE, yaml.safeDump(sortedIcons), 'utf8');
 
     console.log(`Updated icon file with new icon: ${newIconName}`); // eslint-disable-line no-console
+
   }
 };
