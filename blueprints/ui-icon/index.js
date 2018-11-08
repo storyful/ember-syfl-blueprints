e41fa4a0d010809e34fdfeadf89e363662df11c7
Aoife Hannigan <aoife.hannigan@storyful.com>
2018-11-08 09:21:07 +0000

    Check if icon exists then append to file

--- blueprints/ui-icon/index.js
+++ blueprints/ui-icon/index.js
@@ -1,4 +1,7 @@
 /* eslint-env node */
+const fs = require('fs');
+const ICON_FILE = './addon/config/icons.yaml';
+
 module.exports = {
 
   description: 'Adds an icon to the ui-icon mapping.',
@@ -21,11 +24,48 @@ module.exports = {
     }
   ],
 
+  checkIconExists(icon, isFontName){
+    const content = fs.readFileSync(ICON_FILE);
+
+    if(isFontName){
+      return content.toString().indexOf(`: ${icon}`) >= 0
+        || content.toString().indexOf(`name: ${icon}`) >= 0;
+    }
+    else{
+      return content.toString().indexOf(`${icon}:`) >= 0;
+    }
+    
+  },
+
   beforeInstall(options) {
     const newIconName = options.entity.name;
     const iconFontName = options.fontName;
     const modifier = options.modifier;
     const isSyflIcon = options.syflIcon;
 
+    if(this.checkIconExists(newIconName) ){
+      console.log('Icon mapping already exists');
+      return;
+    }
+
+    if(iconFontName && this.checkIconExists(iconFontName, true)){
+      console.log('Icon Font Name already mapped');
+      return;
+    }
+
+    let text = `\n${newIconName}:`;
+    if(isSyflIcon){
+      text = `${text}\n  name: ${newIconName}\n  font-prefix: syfl-icon\n  font: syfl-icon-font`;
+    }
+    else{
+      text = `${text}\n  name: ${iconFontName}`;
+      if(modifier){
+        text = `${text}\n  modifier: ${modifier}`;
+      }
+    }
+
+    fs.appendFileSync(ICON_FILE, text);
+
+    console.log(`Updated icon file with new icon: ${newIconName}`);
   }
 };
