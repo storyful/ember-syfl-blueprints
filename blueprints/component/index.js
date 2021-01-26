76f66ece2498b2b93e8ccb2723a48d682f3d0147
Aoife Hannigan <aoife.hannigan@storyful.com>
2021-01-26 17:01:58 +0000

    Revert ember-cli-string-utils

--- blueprints/component/index.js
+++ blueprints/component/index.js
@@ -5,7 +5,7 @@
 /* eslint-disable */
 const fs = require('fs');
 const path = require('path');
-const stringUtil = require('@ember/string');
+const stringUtil = require('ember-cli-string-utils');
 const pathUtil = require('ember-cli-path-utils');
 const validComponentName = require('ember-cli-valid-component-name');
 const getPathOption = require('ember-cli-get-component-path-option');
